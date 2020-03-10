5585e4d0b387a016a01cdbbc88db4ef5161268cd
Estelle TURBATTE <estelle@turbatte.fr>
2020-03-10 18:21:28 +0100

    Création LoginDoctorActivity

--- app/src/main/java/fr/isen/turbatte/mondossiermedical/HomeActivity.kt
+++ app/src/main/java/fr/isen/turbatte/mondossiermedical/HomeActivity.kt
@@ -1,12 +1,20 @@
 package fr.isen.turbatte.mondossiermedical
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_home.*
 
 class HomeActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_home)
+    doctorButton.setOnClickListener{
+        val intent = Intent(this, LoginDoctorActivity::class.java)
+        //intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP or Intent.FLAG_ACTIVITY_NEW_TASK)
+        startActivity(intent)
     }
+    }
+
 }
---
+++ app/src/main/java/fr/isen/turbatte/mondossiermedical/LoginDoctorActivity.kt
@@ -0,0 +1,12 @@
+package fr.isen.turbatte.mondossiermedical
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class LoginDoctorActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login_doctor)
+    }
+}
