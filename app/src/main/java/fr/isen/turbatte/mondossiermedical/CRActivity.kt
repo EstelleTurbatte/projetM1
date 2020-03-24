cc7240ad08c932e47abee963b711ec71528e7b51
Estelle TURBATTE <estelle@turbatte.fr>
2020-03-24 14:38:08 +0100

    changement activités nouvelles ordonnances et new cr

--- app/src/main/java/fr/isen/turbatte/mondossiermedical/CRActivity.kt
+++ app/src/main/java/fr/isen/turbatte/mondossiermedical/CRActivity.kt
@@ -1,7 +1,9 @@
 package fr.isen.turbatte.mondossiermedical
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_cr.*
 
 class
 CRActivity : AppCompatActivity() {
@@ -9,5 +11,10 @@ CRActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_cr)
+
+        newCRButton.setOnClickListener {
+            val intent = Intent(this, NewCRActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
---
+++ app/src/main/java/fr/isen/turbatte/mondossiermedical/NewCRActivity.kt
@@ -0,0 +1,12 @@
+package fr.isen.turbatte.mondossiermedical
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class NewCRActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_new_cr)
+    }
+}
